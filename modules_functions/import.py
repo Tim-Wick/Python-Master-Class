b1a41c52818c9fab2a9cbfb2099bbfb96741122a
Tim-Wick <tjwick77@gmail.com>
2018-10-24 09:06:47 -0700

    Create import.py

---
+++ modules_functions/import.py
@@ -0,0 +1,13 @@
+import turtle
+
+###Importing one function from turtle at a time###
+#from turtle import forward
+# noinspection PyUnresolvedReferences
+###IntelliJ has bug w/ turtle.###
+
+turtle.forward(150)
+turtle.right(250)
+turtle.forward(150)
+turtle.circle(50)
+
+turtle.done()
