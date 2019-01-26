c954355ef3680cf612c1fe9792598fc7c6137482
Tim-Wick <tjwick77@gmail.com>
2019-01-26 14:42:01 -0800

    Create named_tuples_test.py

---
+++ generators_comprehensions_lambda/named_tuples_test.py
@@ -0,0 +1,9 @@
+from named_tuples import basic_plants_list, plants_list
+
+print(plants_list[0])
+
+# for plant in basic_plants_list:
+#     print(plant[0])
+
+for plant in plants_list:
+    print(plant.name)
